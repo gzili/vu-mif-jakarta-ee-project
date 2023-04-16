2e75328757297e5c7cf53d38bb882636d0a98aa2
gzili <gustas.zilinskas@gmail.com>
2023-04-16 12:44:59 +0300

    java: add logging to UpdateChecker

--- java-ee/src/main/java/lt/vu/mif/api/services/UpdateChecker.java
+++ java-ee/src/main/java/lt/vu/mif/api/services/UpdateChecker.java
@@ -3,14 +3,21 @@ package lt.vu.mif.api.services;
 import jakarta.enterprise.context.ApplicationScoped;
 
 import java.util.Random;
+import java.util.logging.Logger;
 
 @ApplicationScoped
 public class UpdateChecker {
+    private final Logger logger = Logger.getLogger(UpdateChecker.class.getCanonicalName());
+
     public int getUpdateCount() {
+        logger.info("initiating update check");
         try {
             Thread.sleep(5000);
         } catch (InterruptedException ignored) {}
 
-        return new Random().nextInt(5);
+        int updateCount = new Random().nextInt(5);
+        logger.info("found " + updateCount + " updates");
+
+        return updateCount;
     }
 }
